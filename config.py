135dac72a6422e3bb3a93dafc4e8fd22ce1fbb36
EricHo <ericho@hkasianark.com>
2019-01-07 18:13:52 +0800

    improve

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-# projectDir = '/Users/ericho/workspace/JX_App_Android/app/src/main/java/com/hec/app'
-projectDir = ''
-subPackage = ''
+projectDir = '/Users/ericho/workspace/JX_App_Android/app/src/main/java/com/hec/app/'
+# projectDir = ''
+# subPackage = 'mvp/'
 
 BaseFragmentName = 'MvpBaseFragment'
 BaseActivityName = 'BaseActivity'
--- generateMvpFiles.py
+++ generateMvpFiles.py
@@ -2,6 +2,8 @@ from config import *
 import os
 import sys
 
+print('working on: ' + projectDir)
+
 FRAGMENT = '1'
 ACTIVITY = '2'
 
@@ -13,6 +15,10 @@ className = input()
 
 print('Module Type: 1: fragment, 2: activity')
 PageType = input()
+PageType.replace(' ', '')
+
+print('Sub package name: ')
+subPackage = input()
 
 
 def get_page_type(PageType) -> str:
@@ -25,10 +31,20 @@ def get_page_type(PageType) -> str:
         sys.exit()
 
 
-newPackagePath = projectDir + className.lower() + '/'
+def get_new_package_name():
+    subpackage_copy = subPackage
+    if subpackage_copy is not '':
+        if not subpackage_copy.endswith('/'):
+            subpackage_copy += '/'
+        return projectDir + subpackage_copy + className.lower() + '/'
+    else:
+        return projectDir + className.lower() + '/'
+
+
+newPackagePath = get_new_package_name()
 main_class_name = className + get_page_type(PageType)
 mvp_presenter_class_name = className + 'MvpPresenter'
-mvp_view_class_name = className + 'MVPView'
+mvp_view_class_name = className + 'MvpView'
 presenter_class_name = className + 'Presenter'
 if not os.path.exists(newPackagePath):
     os.makedirs(newPackagePath)
